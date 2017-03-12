eaf6877c269aa89c9a1b6bf852de7440468f2b51
saralein <sara.m.trujillo@gmail.com>
2017-03-11 23:26:22 -0600

    Add signal logic.

--- public/game.js
+++ public/game.js
@@ -1,11 +1,28 @@
 let width = 1400,
     height = 600,
-    // alienPink,
-    // alienYellow,
-    // circle0,
-    // circle1,
     currentTempo = 120,
-    meteorPressed = false;
+    meteorPressed = false,
+    synthNotes = ["C2", "E2", "G2", "A2",
+                  "C3", "D3", "E3", "G3",
+                  "A3", "B3", "C4", "D4",
+                  "E4", "G4", "A4", "B4",
+                  "C5"],
+    lastSynthNote = synthNotes[0],
+    signalActive = false,
+    hyperdrive,
+    docking,
+    circle0,
+    circle1,
+    circle2,
+    circle3,
+    meteor,
+    station,
+    alienPink,
+    alienYellow,
+    alienGreen,
+    alienBlue,
+    alienBeige,
+    timer;
 
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'station', { preload: preload, create: create, update: update });
 
@@ -50,7 +67,7 @@ var rock = new Tone.DuoSynth({
     "voice1" : {
       "volume" : -25,
       "oscillator" : {
-        "type" : "sawtooth"
+        "type" : "square"
       },
       "filter" : {
         "Q" : 2,
@@ -74,27 +91,45 @@ var rock = new Tone.DuoSynth({
     }
   }).toMaster();
 
+// var reverb = new Tone.Freeverb().toMaster();
+// rock.connect(reverb);
 //station beat
-// var station = new Tone.Loop(function(time){
-//   synth.triggerAttackRelease("F5", "8t", time)
-// }, "4:4");
-
-// station.start();
+var stationVoice = new Tone.Loop(function(time){
+  synth.triggerAttackRelease("F5", "8t", time)
+}, "2:4");
 
 //pink alien
 
-  var kick = new Tone.MembraneSynth({
-    "envelope" : {
-      "sustain" : 0,
-      "attack" : 0.02,
-      "decay" : 0.8
-    },
-    "octaves" : 10
-  }).toMaster();
+var kick = new Tone.MembraneSynth({
+  "envelope" : {
+    "sustain" : 0,
+    "attack" : 0.02,
+    "decay" : 0.8
+  },
+  "octaves" : 10
+}).toMaster();
 
-  var pinkVoice = new Tone.Loop(function(time){
-    kick.triggerAttackRelease("C2", "16n", time);
-  }, "2n");
+// var pinkVoice = new Tone.Loop(function(time){
+//   kick0.triggerAttackRelease("C2", "32n", time);
+// }, "(@1m) + 4n");
+
+// var pinkVoice = new Tone.Loop(function(time) {
+//   kick.triggerAttackRelease("C2", "4n", time);
+// }, "0:3:0");
+
+
+// var pinkVoice = new Tone.Pattern(function(time, note){
+//     kick.triggerAttackRelease(note, 0.75);
+// }, [["C2"], ["C2"], ["C2"], ["C2"]]);
+
+
+var pinkVoice = new Tone.Loop(function(time) {
+  kick.triggerAttackRelease("C2", "32n", time);
+}, "0:1:0");
+
+var pinkVoice1 = new Tone.Loop(function(time) {
+  kick.triggerAttackRelease("C2", "32n", time);
+}, "0:2:0");
 
 //yellow alien voice
 var yellowVoice = new Tone.Event(function(time, pitch) {
@@ -130,7 +165,7 @@ greenVoice.set({
 
 var blueVoice = new Tone.Loop(function(time){
       snare.triggerAttack(time);
-    }, "2n");
+    }, "0:1:1");
 
 // var blueVoice = new Tone.Loop(function(){
 //   chord.triggerAttackRelease(["C4", "E4", "A4"], "32n + 4t");
@@ -159,54 +194,86 @@ function preload() {
   game.load.image('alienBlueLogo', 'images/aliens/alienBlue_port.png');
   game.load.image('station', 'images/station.png');
   game.load.image('meteor', 'images/meteor.png');
-  game.load.image('play', 'images/play.png');
+  game.load.image('increase', 'images/increase.png');
+  game.load.image('decrease', 'images/decrease.png');
 }
 
 function create() {
-  fromEdge = 1200;
+
   game.renderer.clearBeforeRender = false;
   game.renderer.roundPixels = true;
+  game.stage.backgroundColor = "#111111";
 
   circle0 = game.add.graphics(0, 0);
-
   circle0.lineStyle(2, 0x333333, 1);
   circle0.drawCircle(game.world.centerX, game.world.centerY, 175);
 
   circle1 = game.add.graphics(0, 0);
-
   circle1.lineStyle(2, 0x333333, 1);
   circle1.drawCircle(game.world.centerX, game.world.centerY, 450);
 
-  var circle2 = game.add.graphics(0, 0);
-
+  circle2 = game.add.graphics(0, 0);
   circle2.lineStyle(2, 0x333333, 1);
   circle2.drawCircle(game.world.centerX, game.world.centerY, 800);
 
-  var circle3 = game.add.graphics(0, 0);
-
+  circle3 = game.add.graphics(0, 0);
   circle3.lineStyle(2, 0x333333, 1);
   circle3.drawCircle(game.world.centerX, game.world.centerY, 1250);
 
-  station = game.add.sprite(game.world.centerX - 62, game.world.centerY - 175, 'station');
+  station = game.add.sprite(game.world.centerX, game.world.centerY, 'station');
+  station.anchor.setTo(0.5, 0.5);
+  station.inputEnabled = true;
+  station.events.onInputDown.add(signal, this)
 
-  play = game.add.sprite(game.world.centerX, 540, 'play');
-  play.anchor.x = 0.5;
-  play.scale.setTo(0.5, 0.5);
+  let increase = game.add.sprite(game.world.centerX + 100, 560, 'increase');
+      increase.anchor.setTo(0.5, 0.5);
+      increase.scale.setTo(0.35, 0.35);
+      increase.inputEnabled = true;
+      increase.input.useHandCursor = true;
+      increase.events.onInputDown.add(() => tempo('increase'), this);
+
+  let style = {font: "18px 'VT323'", fill: "#ffffff"};
+
+  hyperdrive = game.add.text(game.world.centerX + 170, 563, "hyperdrive", style);
+  hyperdrive.anchor.set(0.5, 0.5);
+  hyperdrive.visible = false;
+
+  docking = game.add.text(game.world.centerX - 170, 563, "docking", style);
+  docking.anchor.set(0.5, 0.5);
+  docking.visible = false;
+
+  let decrease = game.add.sprite(game.world.centerX - 100, 560, 'decrease');
+      decrease.anchor.setTo(0.5, 0.5);
+      decrease.scale.setTo(0.35, 0.35);
+      decrease.inputEnabled = true;
+      decrease.input.useHandCursor = true;
+      decrease.events.onInputDown.add(() => tempo('decrease'), this);
+
   meteor = game.add.sprite(145, 450, 'meteor');
   meteor.scale.setTo(0.5, 0.5);
-  alienPinkPort = game.add.sprite(fromEdge, game.world.centerY - 173.5, 'alienPinkLogo');
-  alienYellowPort = game.add.sprite(fromEdge, game.world.centerY - 98.5, 'alienYellowLogo');
-  alienGreenPort = game.add.sprite(fromEdge, game.world.centerY - 23.5, 'alienGreenLogo');
-  alienBluePort = game.add.sprite(fromEdge, game.world.centerY + 51.5, 'alienBlueLogo');
-  alienBeigePort = game.add.sprite(fromEdge, game.world.centerY + 126.5, 'alienBeigeLogo');
-  game.stage.backgroundColor = "#111111";
-  //game.physics.enable(alienGreen, Phaser.Physics.ARCADE);
+
+  let alienPinkPort = game.add.sprite(1225, game.world.centerY - 150, 'alienPinkLogo');
+      alienPinkPort.anchor.setTo(0.5, 0.5);
+
+  let alienYellowPort = game.add.sprite(1224, game.world.centerY - 75, 'alienYellowLogo');
+      alienYellowPort.anchor.setTo(0.5, 0.5);
+
+  let alienGreenPort = game.add.sprite(1225, game.world.centerY, 'alienGreenLogo');
+      alienGreenPort.anchor.setTo(0.5, 0.5);
+
+  let alienBluePort = game.add.sprite(1225, game.world.centerY + 75, 'alienBlueLogo');
+      alienBluePort.anchor.setTo(0.5, 0.5);
+
+  let alienBeigePort = game.add.sprite(1225, game.world.centerY + 150, 'alienBeigeLogo');
+      alienBeigePort.anchor.setTo(0.5, 0.5);
 
   //aliens in space
   alienPink = game.add.sprite(920, 75, 'alienPink');
   alienPink.visible = false;
   alienPink.anchor.x = 0.5;
   alienPink.anchor.y = 0.5;
+  alienPink.inputEnabled = true;
+  alienPink.input.enableDrag(true);
 
   alienYellow = game.add.sprite(100, 100, 'alienYellow');
   alienYellow.visible = false;
@@ -247,12 +314,9 @@ function create() {
   meteor.events.onInputDown.add(startMeteor, this);
   meteor.events.onInputUp.add(stopMeteor, this);
 
-  alienPink.inputEnabled = true;
-  alienPink.input.enableDrag(true);
-
   alienYellow.inputEnabled = true;
   alienYellow.input.enableDrag(true);
-  alienYellow.events.onInputUp.add(mouseCords, this);
+  //alienYellow.events.onInputUp.add(mouseCords, this);
 
   alienGreen.inputEnabled = true;
   alienGreen.input.enableDrag(true);
@@ -264,15 +328,112 @@ function create() {
   alienBeige.input.enableDrag(true);
 
   Tone.Transport.start('+0.1');
+  Tone.Transport.bpm.rampTo(currentTempo, 0);
+
+  // timer = game.time.create();
+  // timer.start();
+}
+
+function tempo(action) {
+  if (action === 'increase' && currentTempo < 300) {
+    currentTempo += 30;
+    docking.visible = false;
+  } else if (action === 'decrease' && currentTempo > 60) {
+    currentTempo -= 30
+    hyperdrive.visible = false;
+  }
+  if (currentTempo === 300) {
+    hyperdrive.visible = true;
+  }
+  if (currentTempo === 60) {
+    docking.visible = true;
+  }
   Tone.Transport.bpm.rampTo(currentTempo, 4);
 }
 
-var synthNotes = ["C2", "E2", "G2", "A2",
-                  "C3", "D3", "E3", "G3",
-                  "A3", "B3", "C4", "D4",
-                  "E4", "G4", "A4", "B4",
-                  "C5"];
-var lastSynthNote = synthNotes[0];
+function signal() {
+  if (!signalActive) {
+    stationVoice.start();
+    redrawCircles(0x888888);
+  } else if (signalActive) {
+    stationVoice.stop();
+    redrawCircles(0x333333);
+  }
+  signalActive = !signalActive;
+}
+
+function activeCircles() {
+
+  let time = this.game.time.totalElapsedSeconds() * 1000
+
+  window.setInterval(function() {
+    circle0 = game.add.graphics(0, 0);
+    circle0.lineStyle(2, 0x888888, 1);
+    circle0.drawCircle(game.world.centerX, game.world.centerY, 175);
+    bringUp();
+  }, 0);
+
+  window.setInterval(function() {
+    console.log('hi');
+    circle1 = game.add.graphics(0, 0);
+    circle1.lineStyle(2, 0x888888, 1);
+    circle1.drawCircle(game.world.centerX, game.world.centerY, 450);
+    bringUp();
+  }, time + 1000);
+
+  window.setInterval(function() {
+    console.log('hi');
+    circle2 = game.add.graphics(0, 0);
+    circle2.lineStyle(2, 0x888888, 1);
+    circle2.drawCircle(game.world.centerX, game.world.centerY, 800);
+    bringUp();
+  }, time + 2000);
+
+  window.setInterval(function() {
+    console.log('hi');
+    circle3 = game.add.graphics(0, 0);
+    circle3.lineStyle(2, 0x888888, 1);
+    circle3.drawCircle(game.world.centerX, game.world.centerY, 1250);
+    bringUp();
+  }, time + 3000);
+
+  window.setInterval(function() {
+    redrawCircles(0x333333);
+  }, time + 4000);
+
+}
+
+function redrawCircles(color) {
+
+  circle0 = game.add.graphics(0, 0);
+  circle0.lineStyle(2, color, 1);
+  circle0.drawCircle(game.world.centerX, game.world.centerY, 175);
+
+  circle1 = game.add.graphics(0, 0);
+  circle1.lineStyle(2, color, 1);
+  circle1.drawCircle(game.world.centerX, game.world.centerY, 450);
+
+  circle2 = game.add.graphics(0, 0);
+  circle2.lineStyle(2, color, 1);
+  circle2.drawCircle(game.world.centerX, game.world.centerY, 800);
+
+  circle3 = game.add.graphics(0, 0);
+  circle3.lineStyle(2, color, 1);
+  circle3.drawCircle(game.world.centerX, game.world.centerY, 1250);
+
+  bringUp();
+
+}
+
+function bringUp() {
+  game.world.bringToTop(station);
+  game.world.bringToTop(alienPink);
+  game.world.bringToTop(alienYellow);
+  game.world.bringToTop(alienGreen);
+  game.world.bringToTop(alienBlue);
+  game.world.bringToTop(alienBeige);
+  game.world.bringToTop(meteor);
+}
 
 function startMeteor () {
   meteorPressed = true;
@@ -283,15 +444,14 @@ function stopMeteor() {
   rock.triggerRelease();
 }
 
-function mouseCords() {
-  console.log(game.input.mousePointer.x, game.input.mousePointer.y);
-}
+// function mouseCords() {
+//   console.log(game.input.mousePointer.x, game.input.mousePointer.y);
+// }
 
 function generateSprite(sprite, voice) {
   sprite.visible = !sprite.visible;
   if (sprite.visible) {
-    voice.start("+1m");
-    //voice.bpm.rampTo(currentTempo, 4)
+    voice.start("(@1m)");
   } else {
     voice.stop();
   }
